82d517ff902770cab151677551f0424210e3033a
unknown <Emmanuel.o.irabor@gmail.com>
2026-06-24 22:36:04 +0100

    reduced splaash screen logo

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function SplashPage() {
     <div className="flex flex-col justify-center items-center h-screen gap-8">
       <Image
         src={logo}
-        alt="Duel Domain Logo"
-        className="xl:w-20 xl:h-20 w-25 h-25"
+        alt="Voice Guard pay logo"
+        className="xl:w-20 xl:h-20 w-15 h-15"
       />
 
         <p className="text-primary-500 font-semibold">Voice Guard Pay</p>
