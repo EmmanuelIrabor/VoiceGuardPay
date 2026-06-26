abe707e66e57e2d71526d1fe4a5bd96fd3ce5733
unknown <Emmanuel.o.irabor@gmail.com>
2026-06-26 08:46:04 +0100

    translate

--- components/T.tsx
+++ components/T.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useTranslate } from "@/lib/hooks/useTranslate";
-
 export default function T({ children }: { children: string }) {
   const translated = useTranslate(children);
   return <>{translated}</>;
